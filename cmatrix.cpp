fdca9b59ef16f2f20127a7f65223df8744f071f4
igg@cathilya.org <igg@cathilya.org@d860a611-dd6a-fbde-3e4e-7fa7191099b5>
2011-04-22 19:46:22 +0000

    Cleaning out warnings in cmatrix
    git-svn-id: http://wnd-charm.googlecode.com/svn/wndchrm/trunk@220 d860a611-dd6a-fbde-3e4e-7fa7191099b5

--- cmatrix.cpp
+++ cmatrix.cpp
@@ -29,7 +29,9 @@
 
 
 
+#ifdef WIN32
 #pragma hdrstop
+#endif
 
 #include <math.h>
 #include <stdio.h>
@@ -62,7 +64,7 @@
 
 RGBcolor HSV2RGB(HSVcolor hsv)
 {   RGBcolor rgb;
-    float R, G, B;
+    float R=0, G=0, B=0;
     float H, S, V;
     float i, f, p, q, t;
 
@@ -111,6 +113,7 @@ HSVcolor RGB2HSV(RGBcolor rgb)
     hsv.saturation = 0;
   if (hsv.saturation == 0) hsv.hue = 0; //-1;
   else {
+  	h = 0;
     if (r == max)
       h = (g - b) / delta;
     else if (g == max)
@@ -118,6 +121,7 @@ HSVcolor RGB2HSV(RGBcolor rgb)
     else if (b == max)
       h = 4 + (r - g) / delta;
     h *= 60.0;
+    if (h >= 360) h -= 360.0;
     if (h < 0.0) h += 360.0;
     hsv.hue = (byte)(h *(240.0/360.0));
   }
@@ -188,16 +192,16 @@ int ImageMatrix::LoadBMP(char *filename,int ColorMode)
 int ImageMatrix::LoadTIFF(char *filename)
 {
 //#ifndef WIN32
-   unsigned long h,w,x,y,z;
+   int h,w,x,y,z;
    unsigned short int spp,bps;
    TIFF *tif = NULL;
    //tdata_t buf;
    unsigned char *buf8;
    unsigned short *buf16;
    double max_val;
-   pix_data pix;
+   pix_data pix={{{0}},0};
    TIFFSetWarningHandler(NULL);
-   if (tif = TIFFOpen(filename, "r"))
+   if ( (tif = TIFFOpen(filename, "r")) )
    {
      TIFFGetField(tif, TIFFTAG_IMAGEWIDTH, &w);
      width = w;
@@ -227,7 +231,7 @@ int ImageMatrix::LoadTIFF(char *filename)
             while (x<width)
             { unsigned char byte_data;
               unsigned short short_data;
-              double val;
+              double val=0;
               int sample_index;
               for (sample_index=0;sample_index<spp;sample_index++)
               {  byte_data=buf8[col+sample_index];
@@ -238,7 +242,7 @@ int ImageMatrix::LoadTIFF(char *filename)
                  {  if (sample_index==0) pix.clr.RGB.red=(unsigned char)(255*(val/max_val));
                     if (sample_index==1) pix.clr.RGB.green=(unsigned char)(255*(val/max_val));
                     if (sample_index==2) pix.clr.RGB.blue=(unsigned char)(255*(val/max_val));
-                    if (ColorMode=cmHSV) pix.clr.HSV=RGB2HSV(pix.clr.RGB);
+                    if ( ColorMode==cmHSV ) pix.clr.HSV=RGB2HSV(pix.clr.RGB);
                  }
               }
               if (spp==3) pix.intensity=COLOR2GRAY(RGB2COLOR(pix.clr.RGB));
@@ -308,7 +312,7 @@ int ImageMatrix::SaveTiff(char *filename)
 
 int ImageMatrix::LoadPPM(char *filename, int ColorMode)
 {  FILE *fi;
-   char ty[256],line[256],*p;
+   char ty[256],line[256];
    byte *buffer;
    int x, y, w, h, m=-1;
    pix_data pix;
@@ -498,7 +502,7 @@ void ImageMatrix::diff(ImageMatrix *matrix)
        {  pix_data pix1,pix2;
           pix1=pixel(x,y,z);
           pix2=matrix->pixel(x,y,z);
-          pix1.intensity==fabs(pix1.intensity-pix2.intensity);
+          pix1.intensity=fabs(pix1.intensity-pix2.intensity);
           pix1.clr.RGB.red=(byte)fabs(pix1.clr.RGB.red-pix2.clr.RGB.red);		  
           pix1.clr.RGB.green=(byte)fabs(pix1.clr.RGB.green-pix2.clr.RGB.green);		  
           pix1.clr.RGB.blue=(byte)fabs(pix1.clr.RGB.blue-pix2.clr.RGB.blue);
@@ -877,7 +881,8 @@ for (int k=-depth2;k<=depth2;++k) {
 */
 
 void ImageMatrix::GetColorStatistics(double *hue_avg, double *hue_std, double *sat_avg, double *sat_std, double *val_avg, double *val_std, double *max_color, double *colors)
-{  long x,y,a,color_index;
+{
+	long a,color_index;
    color hsv;
    double max,pixel_num;
    float certainties[COLORS_NUM+1];
@@ -941,7 +946,7 @@ void ImageMatrix::GetColorStatistics(double *hue_avg, double *hue_std, double *s
    grey level represents a different color
 */
 void ImageMatrix::ColorTransform(double *color_hist, int use_hue)
-{  long x,y,z,base_color;
+{  long x,y,z;
    double cb_intensity;
    double max_value;
    pix_data hsv_pixel;
@@ -1076,13 +1081,13 @@ void ImageMatrix::ChebyshevTransform(int N)
 {  double *out;
    int x,y,old_width;
 
-   if (N<2) N=min(width,height);
+   if (N<2) N=MIN(width,height);
    out=new double[height*N];
    Chebyshev2D(this, out,N);
 
    old_width=width;  /* keep the old width to free the memory */
    width=N;
-   height=min(height,N);   /* prevent error */
+   height=MIN(height,N);   /* prevent error */
 
    for(y=0;y<height;y++)
      for(x=0;x<width;x++)
@@ -1096,7 +1101,7 @@ void ImageMatrix::ChebyshevTransform(int N)
 void ImageMatrix::ChebyshevFourierTransform2D(double *coeff)
 {  ImageMatrix *matrix;
    matrix=duplicate();
-   if (width*height>300*300) matrix->Downsample(min(300.0/(double)width,300.0/(double)height),min(300.0/(double)width,300.0/(double)height));  /* downsample for avoiding memory problems */
+   if (width*height>300*300) matrix->Downsample(MIN(300.0/(double)width,300.0/(double)height),MIN(300.0/(double)width,300.0/(double)height));  /* downsample for avoiding memory problems */
    ChebyshevFourier2D(matrix, 0, coeff,32);
    delete matrix;
 }
@@ -1148,7 +1153,7 @@ void ImageMatrix::Symlet5Transform()
 void ImageMatrix::ChebyshevStatistics2D(double *coeff, int N, int bins_num)
 {
    if (N<2) N=20;
-   if (N>min(width,height)) N=min(width,height);   
+   if (N>MIN(width,height)) N=MIN(width,height);   
    ChebyshevTransform(N);
    histogram(coeff,bins_num,0);
 }
@@ -1179,10 +1184,10 @@ void ImageMatrix::EdgeTransform()
      for (x=0;x<TempMatrix->width;x++)
        for (z=0;z<TempMatrix->depth;z++)	 
        {  double max_x=0,max_y=0,max_z=0;
-          if (y>0 && y<height-1) max_y=max(fabs(TempMatrix->pixel(x,y,z).intensity-TempMatrix->pixel(x,y-1,z).intensity),fabs(TempMatrix->pixel(x,y,z).intensity-TempMatrix->pixel(x,y+1,z).intensity));
-          if (x>0 && x<width-1) max_x=max(fabs(TempMatrix->pixel(x,y,z).intensity-TempMatrix->pixel(x-1,y,z).intensity),fabs(TempMatrix->pixel(x,y,z).intensity-TempMatrix->pixel(x+1,y,z).intensity));
-          if (z>0 && z<depth-1) max_z=max(fabs(TempMatrix->pixel(x,y,z).intensity-TempMatrix->pixel(x,y,z-1).intensity),fabs(TempMatrix->pixel(x,y,z).intensity-TempMatrix->pixel(x,y,z+1).intensity));
-          SetInt(x,y,z,max(max(max_x,max_z),max_y));
+          if (y>0 && y<height-1) max_y=MAX(fabs(TempMatrix->pixel(x,y,z).intensity-TempMatrix->pixel(x,y-1,z).intensity),fabs(TempMatrix->pixel(x,y,z).intensity-TempMatrix->pixel(x,y+1,z).intensity));
+          if (x>0 && x<width-1) max_x=MAX(fabs(TempMatrix->pixel(x,y,z).intensity-TempMatrix->pixel(x-1,y,z).intensity),fabs(TempMatrix->pixel(x,y,z).intensity-TempMatrix->pixel(x+1,y,z).intensity));
+          if (z>0 && z<depth-1) max_z=MAX(fabs(TempMatrix->pixel(x,y,z).intensity-TempMatrix->pixel(x,y,z-1).intensity),fabs(TempMatrix->pixel(x,y,z).intensity-TempMatrix->pixel(x,y,z+1).intensity));
+          SetInt(x,y,z,MAX(MAX(max_x,max_z),max_y));
        }
 
    /* use otsu global threshold to set edges to 0 or 1 */
@@ -1201,7 +1206,6 @@ void ImageMatrix::EdgeTransform()
 /* transform by gradient magnitude */
 void ImageMatrix::GradientMagnitude(int span)
 {  long x,y,z;
-   double sum;
    if (span==0) span=2;  /* make sure 0 is not a default */
    for (x=0;x<width-span;x++)
      for (y=0;y<height-span;y++)
@@ -1352,7 +1356,6 @@ void ImageMatrix::RadonTransform2D(double *vec)
     double *pixels,*ptr,bins[3];
     int angle,num_angles=4;
     double theta[4]={0,45,90,135};
-    double min,max;
     int rLast,rFirst;
     rLast = (int) ceil(sqrt(pow(width-1-(width-1)/2,2)+pow(height-1-(height-1)/2,2))) + 1;
     rFirst = -rLast;
@@ -1400,7 +1403,7 @@ void ImageMatrix::RadonTransform2D(double *vec)
    Find otsu threshold
 */
 double ImageMatrix::Otsu()
-{  long a,x,y;
+{  long a;
    double hist[256],omega[256],mu[256],sigma_b2[256],maxval=-INF,sum,count;
    double max=pow(2,bits)-1;
    histogram(hist,256,1);
@@ -1643,7 +1646,7 @@ void ImageMatrix::zernike2D(double *zvalues, long *output_size)
    
 void ImageMatrix::fractal2D(int bins,double *output)
 {  int x,y,k,bin=0;
-   int K=min(width,height)/5;
+   int K=MIN(width,height)/5;
    int step=(long)floor(K/bins);
    if (step<1) step=1;   /* avoid an infinite loop if the image is small */
    for (k=1;k<K;k=k+step)
@@ -1658,8 +1661,7 @@ void ImageMatrix::fractal2D(int bins,double *output)
    }
 }
 
+
+#ifdef WIN32
 #pragma package(smart_init)
-
-
-
-
+#endif
