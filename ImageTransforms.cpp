cd92bf91c0e66c1b663245b9d0be9a393ddf70be
igg@nih.gov <igg@nih.gov@d860a611-dd6a-fbde-3e4e-7fa7191099b5>
2013-02-26 05:11:01 +0000

    Oops forgot to add ImageTransforms (renamed from FeatureTransforms) and SharedImageMatrix factored out of ImageTransforms
    git-svn-id: http://wnd-charm.googlecode.com/svn/wndchrm/trunk@589 d860a611-dd6a-fbde-3e4e-7fa7191099b5

---
+++ ImageTransforms.cpp
@@ -0,0 +1,159 @@
+#include <iostream> // used for debug output from instantiator methods
+#include <cmath>
+#include <fcntl.h>
+
+#include "cmatrix.h"
+#include "FeatureNames.h"
+#include "ImageTransforms.h"
+#include "colors/FuzzyCalc.h" // for definition of compiler constant COLORS_NUM
+#include "transforms/fft/bcb_fftw3/fftw3.h"
+
+int ImageTransform::verbosity = 0;
+
+void ImageTransform::print_info() {
+
+}
+
+// storage for static vector of instances
+// Done in a static member function holding a static to avoid "static initialization order fiasco"
+// FIXME: although this heap memory will be allocated before main() entry,
+//   its probably still a good idea to make a destructor to clean it up.
+int ImageTransformInstances::verbosity = 0;
+bool ImageTransformInstances::initialized () {
+	static std::vector<const ImageTransform *> &instances = getInstances();
+	return (!instances.empty());
+}
+std::vector<const ImageTransform *> &ImageTransformInstances::getInstances () {
+	static std::vector<const ImageTransform *> *ImageTransforms = new std::vector<const ImageTransform *>;
+	return (*ImageTransforms);
+}
+bool ImageTransformInstances::add (const ImageTransform *algorithm) {
+	static std::vector<const ImageTransform *> &instances = getInstances();
+
+	if (verbosity > 4) std::cout << "Registering ImageTransform " << algorithm->name << std::endl;
+	instances.insert (instances.end(), algorithm);
+	FeatureNames::registerImageTransform (algorithm);
+	return (true);
+};
+
+
+
+//===========================================================================
+
+void EmptyTransform::execute( const ImageMatrix * matrix_IN, ImageMatrix * matrix_OUT ) const {
+	if( !( matrix_IN && matrix_OUT) )
+		return;
+	matrix_OUT->copy (*matrix_IN);
+	if (verbosity > 3) std::cout << name << " transform." << std::endl;
+}
+
+//===========================================================================
+
+FourierTransform::FourierTransform () : ImageTransform ("Fourier") {};
+
+/* fft 2 dimensional transform */
+// http://www.fftw.org/doc/
+void FourierTransform::execute( const ImageMatrix * matrix_IN, ImageMatrix * matrix_OUT ) const {
+	if( !( matrix_IN && matrix_OUT) )
+		return;
+	
+	matrix_OUT->copy (*matrix_IN);
+	if (verbosity > 3) std::cout << "Performing transform " << name << std::endl;
+	matrix_OUT->fft2();
+}
+
+// Register a static instance of the class using a namespace for the global bool
+namespace ImageTransformReg {
+	static const bool FourierTransformReg = ImageTransformInstances::add (new FourierTransform);
+}
+
+
+//===========================================================================
+
+ChebyshevTransform::ChebyshevTransform () : ImageTransform ("Chebyshev") {};
+
+void ChebyshevTransform::execute( const ImageMatrix * matrix_IN, ImageMatrix * matrix_OUT ) const {
+	if( !( matrix_IN && matrix_OUT) )
+		return;	
+	matrix_OUT->copy (*matrix_IN);
+	if (verbosity > 3) std::cout << "Performing transform " << name << std::endl;
+
+	matrix_OUT->ChebyshevTransform(0);
+}
+
+// Register a static instance of the class using a namespace for the global bool
+namespace ImageTransformReg {
+	static const bool ChebyshevTransformReg = ImageTransformInstances::add (new ChebyshevTransform);
+}
+
+//===========================================================================
+
+WaveletTransform::WaveletTransform () : ImageTransform ("Wavelet") {};
+
+void WaveletTransform::execute( const ImageMatrix * matrix_IN, ImageMatrix * matrix_OUT ) const {
+	if( !( matrix_IN && matrix_OUT) )
+		return;
+	
+	matrix_OUT->copy (*matrix_IN);
+	if (verbosity > 3) std::cout << "Performing transform " << name << std::endl;
+	matrix_OUT->Symlet5Transform();
+}
+
+// Register a static instance of the class using a namespace for the global bool
+namespace ImageTransformReg {
+	static const bool WaveletTransformReg = ImageTransformInstances::add (new WaveletTransform);
+}
+
+//===========================================================================
+
+EdgeTransform::EdgeTransform () : ImageTransform ("Edge") {};
+
+void EdgeTransform::execute( const ImageMatrix * matrix_IN, ImageMatrix * matrix_OUT ) const {
+	if( !( matrix_IN && matrix_OUT) )
+		return;
+	
+	matrix_OUT->copy (*matrix_IN);
+	if (verbosity > 3) std::cout << "Performing transform " << name << std::endl;
+	matrix_OUT->EdgeTransform();
+}
+
+// Register a static instance of the class using a namespace for the global bool
+namespace ImageTransformReg {
+	static const bool EdgeTransformReg = ImageTransformInstances::add (new EdgeTransform);
+}
+
+//===========================================================================
+
+ColorTransform::ColorTransform () : ImageTransform ("Color") {};
+
+void ColorTransform::execute( const ImageMatrix * matrix_IN, ImageMatrix * matrix_OUT ) const {
+	if( !( matrix_IN && matrix_OUT) )
+		return;
+	
+	matrix_OUT->copy (*matrix_IN);
+	if (verbosity > 3) std::cout << "Performing transform " << name << std::endl;
+	matrix_OUT->ColorTransform();
+}
+
+// Register a static instance of the class using a namespace for the global bool
+namespace ImageTransformReg {
+	static const bool ColorTransformReg = ImageTransformInstances::add (new ColorTransform);
+}
+
+//===========================================================================
+
+HueTransform::HueTransform () : ImageTransform ("Hue") {};
+
+void HueTransform::execute( const ImageMatrix * matrix_IN, ImageMatrix * matrix_OUT ) const {
+	if( !( matrix_IN && matrix_OUT) )
+		return;
+	
+	matrix_OUT->copy (*matrix_IN);
+	if (verbosity > 3) std::cout << "Performing transform " << name << std::endl;
+	matrix_OUT->HueTransform();
+}
+
+// Register a static instance of the class using a namespace for the global bool
+namespace ImageTransformReg {
+	static const bool HueTransformReg = ImageTransformInstances::add (new HueTransform);
+}
---
+++ ImageTransforms.h
@@ -0,0 +1,85 @@
+#ifndef __TRANSFORMS_H_
+#define __TRANSFORMS_H_
+
+#include <string>
+#include <vector>
+#include "cmatrix.h"
+
+
+
+/*! ImageTransform
+ *  defines the interface for all inheriting transform classes
+ *  Turns any class that inherits this interface into a singleton
+ */
+class ImageMatrix; // forward declaration
+class ImageTransform {
+	public:
+		std::string name;
+		static int verbosity;
+		virtual void execute( const ImageMatrix * matrix_IN, ImageMatrix * matrix_OUT ) const = 0;
+		void print_info();
+	protected:
+		ImageTransform (const std::string &s) { name = s;}
+		ImageTransform (const char *s) { name = s;}
+		ImageTransform() {};
+};
+
+// The purpose of this is to hold instances of the transform algorithms below
+// so that we can get references (pointers) to them later for looking them up by name using a map.
+// Done in a static member function holding a static to avoid "static initialization order fiasco"
+// FIXME: although this heap memory will be allocated before main() entry,
+//   its probably still a good idea to make a destructor to clean it up.
+class ImageTransformInstances {
+	public:
+		static int verbosity;
+		static bool initialized ();
+		static bool add (const ImageTransform *algorithm);
+		static std::vector<const ImageTransform *> &getInstances ();
+};
+
+class EmptyTransform : public ImageTransform {
+	public:
+		EmptyTransform () : ImageTransform ("Empty") {};
+		EmptyTransform (const std::string &s) : ImageTransform (s) {};
+		EmptyTransform (const char *s) : ImageTransform (s) {};
+		virtual void execute( const ImageMatrix * matrix_IN, ImageMatrix * matrix_OUT ) const;
+};
+
+class FourierTransform : public ImageTransform {
+	public:
+		FourierTransform();
+		virtual void execute( const ImageMatrix * matrix_IN, ImageMatrix * matrix_OUT ) const;
+};
+
+class ChebyshevTransform: public ImageTransform {
+	public:
+		ChebyshevTransform();
+		virtual void execute( const ImageMatrix * matrix_IN, ImageMatrix * matrix_OUT ) const;
+};
+
+class WaveletTransform : public ImageTransform {
+	public:
+		WaveletTransform();
+		virtual void execute( const ImageMatrix * matrix_IN, ImageMatrix * matrix_OUT ) const;
+};
+
+class EdgeTransform : public ImageTransform {
+	public:
+		EdgeTransform();
+		virtual void execute( const ImageMatrix * matrix_IN, ImageMatrix * matrix_OUT ) const;
+};
+
+class ColorTransform : public ImageTransform {
+	public:
+		ColorTransform();
+		virtual void execute( const ImageMatrix * matrix_IN, ImageMatrix * matrix_OUT ) const;
+};
+
+class HueTransform : public ImageTransform {
+	public:
+		HueTransform();
+		virtual void execute( const ImageMatrix * matrix_IN, ImageMatrix * matrix_OUT ) const;
+};
+
+#endif
+
