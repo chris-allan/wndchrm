39322bea0f17ad11801669a4209c3ca6186db21d
igg@nih.gov <igg@nih.gov@d860a611-dd6a-fbde-3e4e-7fa7191099b5>
2013-03-06 19:06:03 +0000

    calling copyFields() in submatrix() to ensure consistency between source and output
    git-svn-id: http://wnd-charm.googlecode.com/svn/wndchrm/trunk@642 d860a611-dd6a-fbde-3e4e-7fa7191099b5

--- cmatrix.cpp
+++ cmatrix.cpp
@@ -381,14 +381,13 @@ void ImageMatrix::copy(const ImageMatrix &copy) {
 void ImageMatrix::submatrix (const ImageMatrix &matrix, const unsigned int x1, const unsigned int y1, const unsigned int x2, const unsigned int y2) {
 	unsigned int x0, y0;
 
-	bits = matrix.bits;
-	ColorMode = matrix.ColorMode;
 	// verify that the image size is OK
 	x0 = (x1 < 0 ? 0 : x1);
 	y0 = (y1 < 0 ? 0 : y1);
 	unsigned int new_width  = (x2 >= matrix.width  ? matrix.width  : x2 - x0 + 1);
 	unsigned int new_height = (y2 >= matrix.height ? matrix.height : y2 - y0 + 1);
 
+	copyFields (matrix);
 	allocate (new_width, new_height);
 	// Copy the Eigen matrixes
 	// N.B. Eigen matrix parameter order is rows, cols, not X, Y
